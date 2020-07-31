bb7af565f9f82461b162e8c247a6c82b7fa3a144
arfeenm <arfeen_malik123@hotmail.com>
2020-07-31 14:36:59 -0400

    Add DI to fragments

--- app/src/main/java/com/corp/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/corp/runningapp/ui/fragments/RunFragment.kt
@@ -1,7 +1,14 @@
 package com.corp.runningapp.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.corp.runningapp.R
+import com.corp.runningapp.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class RunFragment: Fragment(R.layout.fragment_run) {
+
+    private val viewModel: MainViewModel by viewModels()
 }
--- app/src/main/java/com/corp/runningapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/corp/runningapp/ui/fragments/StatisticsFragment.kt
@@ -1,7 +1,15 @@
 package com.corp.runningapp.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.corp.runningapp.R
+import com.corp.runningapp.ui.viewmodels.MainViewModel
+import com.corp.runningapp.ui.viewmodels.StatisticsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class StatisticsFragment: Fragment(R.layout.fragment_statistics) {
+
+    private val viewModel: StatisticsViewModel by viewModels()
+
 }
