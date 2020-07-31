e9b0095b0e7dcf99e4dfb0b659f46c7b51834325
arfeenm <arfeen_malik123@hotmail.com>
2020-07-31 13:47:01 -0400

    Add timber for logging

--- app/src/main/java/com/corp/runningapp/MainActivity.kt
+++ app/src/main/java/com/corp/runningapp/MainActivity.kt
@@ -2,8 +2,17 @@ package com.corp.runningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.corp.runningapp.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDao: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
